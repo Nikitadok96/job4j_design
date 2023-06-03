7eacc5367d3192a258d877e6712564797c059016
Mingrow <nikitosvozh@gmail.com>
2023-06-03 20:30:21 +0300

    6. Читаем файл конфигурации [#858  [#858]]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.path))) {
+            Pattern pattern = Pattern.compile("^.+=\\w.+");
+            values.putAll(bufferedReader.lines()
+                            .filter(s -> {
+                                boolean rsl = s.length() > 0;
+                                if (rsl && !pattern.matcher(s).find()) {
+                                    throw new IllegalArgumentException();
+                                }
+                                return rsl;
+                            })
+                    .map(s -> s.split("=", 2))
+                    .collect(Collectors.toMap(k -> k[0], v -> v[1])));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+        assertThat(config.value("myName")).isEqualTo("Nikita");
+        assertThat(config.value("second")).isEqualTo("lalin=1");
+        assertThat(config.value("key")).isNull();
+    }
+
+    @Test
+    void whenEmptyAndComments() {
+        String path = "./data/empty_and_comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("Comment")).isNull();
+    }
+
+    @Test
+    void whenIllegalArgumentException() {
+        String path = "./data/violation_pattern.properties";
+        Config config = new Config(path);
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(config::load);
+    }
+}
