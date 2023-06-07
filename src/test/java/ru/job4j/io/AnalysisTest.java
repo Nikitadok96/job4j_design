678d96cecc9e04aada249d16780bc85b89c56e2d
Mingrow <nikitosvozh@gmail.com>
2023-06-07 06:08:10 +0300

    8. Тестирование IO [#173905  [#173905]]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+
+import java.io.*;
+import java.nio.file.Path;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class AnalysisTest {
+    @Test
+    void whenOnePeriod(@TempDir Path tempPath) throws IOException {
+        File source = tempPath.resolve("sourcePath.txt").toFile();
+        try (PrintWriter printWriter = new PrintWriter(source)) {
+            printWriter.println("200 10:56:01");
+            printWriter.println("500 10:57:01");
+            printWriter.println("400 10:58:01");
+            printWriter.println("300 10:59:01");
+        }
+        File target = tempPath.resolve("targetPath.txt").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(target))) {
+            bufferedReader.lines().forEach(stringBuilder::append);
+        }
+        assertThat("10:57:01;10:59:01;").hasToString(stringBuilder.toString());
+    }
+}
