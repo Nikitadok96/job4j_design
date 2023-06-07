c05061933ac6f27fb47a0c9b40cefb36a3fc6df7
Mingrow <nikitosvozh@gmail.com>
2023-06-07 19:23:15 +0300

    7. Анализ доступности сервера [#859 #421114]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,27 +5,19 @@ import java.util.regex.Pattern;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(target))) {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
+        try (PrintWriter printWriter = new PrintWriter(target);
+             BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             Pattern pattern = Pattern.compile("^[4, 5]");
             boolean isWorking = true;
             while (bufferedReader.ready()) {
                 String str = bufferedReader.readLine();
-                String message = pattern.matcher(str).find()
-                        ? String.format("%s;", str.split(" ")[1])
-                        : String.format("%s;\n", str.split(" ")[1]);
-                if (pattern.matcher(str).find()) {
-                    if (isWorking) {
-                        printWriter.write(message);
-                        isWorking = false;
-                    }
-                } else {
-                    if (!isWorking) {
-                        printWriter.write(message);
-                        isWorking = true;
-                    }
+                boolean status = pattern.matcher(str).find();
+                if (status == isWorking) {
+                    printWriter.append(str.split(" ")[1])
+                            .append(";")
+                            .append(status ? "" : System.lineSeparator());
+                    isWorking = !isWorking;
                 }
-
             }
 
         } catch (IOException e) {
