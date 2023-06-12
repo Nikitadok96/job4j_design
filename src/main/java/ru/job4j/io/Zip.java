ae83d660df6bc08a14546316569801431c2137f8
Mingrow <nikitosvozh@gmail.com>
2023-06-12 20:49:19 +0300

    14. Архивировать проект [#861  [#861]]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgsName argsName = ArgsName.of(args);
+        List.of("d", "e", "o").forEach(argsName::get);
+        Path sourceFolder = Paths.get(argsName.get("d"));
+        if (!Files.exists(sourceFolder)) {
+            throw new IllegalArgumentException(String.format(
+                    "Source folder: %s is not exist", sourceFolder.toAbsolutePath()
+            ));
+        }
+        List<Path> pathList = Search.search(sourceFolder, path -> !path.toFile().getName().endsWith(argsName.get("e")));
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+        zip.packFiles(pathList, new File(argsName.get("o")));
+    }
+
+
+}
