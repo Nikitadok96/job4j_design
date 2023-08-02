3daaa72d1798ded936348b58eab9357d5f85c145
Mingrow <nikitosvozh@gmail.com>
2023-08-02 09:30:44 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ db/isolate._script.sql
@@ -0,0 +1,5 @@
+insert into products(name, count) values ('Dmitry', 19000);
+insert into products(name, count) values ('Oleg', 20000);
+insert into products(name, count) values ('Nikita', 10000);
+insert into products(name, count) values ('Marina', 2500);
+insert into products(name, count) values ('Semen', 250);
