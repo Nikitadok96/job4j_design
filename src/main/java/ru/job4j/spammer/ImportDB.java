652fc5fbb1db97c72c35613d21127ee52d4dc2a5
Mingrow <nikitosvozh@gmail.com>
2023-08-04 16:10:53 +0300

    0.2. PreparedStatement [#379307  [#379307]]

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,87 @@
+package ru.job4j.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines()
+                    .filter(this::patternFilter)
+                    .forEach(l -> {
+                        User user = new User(
+                                l.split(";", 0)[0],
+                                l.split(";", 0)[1]
+                        );
+                        users.add(user);
+                    });
+        }
+        return users;
+    }
+
+    private boolean patternFilter(String str) {
+        Pattern pattern = Pattern.compile("^.+;\\w.+[;]");
+        boolean rsl = str.isBlank() || !pattern.matcher(str).find();
+        if (rsl) {
+            String patternString = "Pattern violation: %s\nCorrect view: name;email";
+            throw new IllegalArgumentException(String.format(patternString, str));
+        }
+        return !rsl;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("INSERT INTO users(name, email) VALUES (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "data/dump.txt");
+        db.save(db.load());
+    }
+}
