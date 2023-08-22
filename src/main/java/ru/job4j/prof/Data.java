e693099445a20a7fa51b2c1db5b91286125f3755
Mingrow <nikitosvozh@gmail.com>
2023-08-22 22:03:25 +0300

    3. Эксперименты с различными GC [#1590]

---
+++ src/main/java/ru/job4j/prof/Data.java
@@ -0,0 +1,7 @@
+package ru.job4j.prof;
+
+public interface Data {
+    void insert(int elements);
+
+    int[] getClone();
+}
---
+++ src/main/java/ru/job4j/prof/Sort.java
@@ -0,0 +1,5 @@
+package ru.job4j.prof;
+
+public interface Sort {
+    boolean sort(Data data);
+}
