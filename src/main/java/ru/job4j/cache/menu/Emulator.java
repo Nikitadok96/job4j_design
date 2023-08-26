b6d2a98adaeeff1867ecb945bae4afc204251f05
Mingrow <nikitosvozh@gmail.com>
2023-08-26 16:12:26 +0300

    1. Реализация кеша на SoftReference [#1592 #439844]

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -1,25 +1,61 @@
 package ru.job4j.cache.menu;
 
 import ru.job4j.cache.AbstractCache;
+import ru.job4j.cache.DirFileCache;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Emulator {
-    private String cacheDir;
-    private final AbstractCache<String, String> abstractCache;
 
-    public Emulator(String cacheDir, AbstractCache<String, String> abstractCache) {
-        this.cacheDir = cacheDir;
-        this.abstractCache = abstractCache;
+    private static String cacheDir = null;
+    public static final String MENU = """
+                Введите 1, чтобы указать кэшируемую директорию.
+                Введите 2, чтобы загрузить содержимое файла в кэш.
+                Введите 3, получить содержимое файла из кэша.
+                Введите любое другое число для выхода.
+            """;
+    public static final String SELECT = "Выберите меню: ";
+
+    public static void main(String[] args) {
+        boolean run = true;
+        AbstractCache<String, String> abstractCache = null;
+        while (run) {
+            System.out.println(MENU);
+            System.out.println(SELECT);
+            int userChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+            if (1 == userChoice) {
+                System.out.print("Укажите директорию: ");
+                cacheDir = new Scanner(System.in).nextLine();
+                abstractCache = new DirFileCache(cacheDir);
+            } else if (2 == userChoice) {
+                if (dirIsReady()) {
+                    System.out.print("Введите наименование файла: ");
+                    String key = new Scanner(System.in).nextLine();
+                    System.out.print("Введите содержание файла: ");
+                    String value = new Scanner(System.in).nextLine();
+                    abstractCache.put(key, value);
+                }
+            } else if (3 == userChoice) {
+                if (dirIsReady()) {
+                    System.out.print("Введите наименование файла: ");
+                    String file = new Scanner(System.in).nextLine();
+                    System.out.println(abstractCache.get(file));
+                }
+            } else {
+                run = false;
+                System.out.println("EXIT");
+            }
+        }
     }
 
-    public void setCachedDir(String dir) {
-        this.cacheDir = dir;
-    }
-
-    public String getCacheData(String key) {
-        return abstractCache.get(key);
-    }
-
-    public void putCacheData(String key, String value) {
-        abstractCache.put(key, value);
+    private static boolean dirIsReady() {
+        boolean rsl = false;
+        if (cacheDir != null) {
+            rsl = true;
+        } else {
+            System.out.println("Необходимо указать директорию.");
+        }
+        return rsl;
     }
 }
